85f6d4ee247f9e14c5188c7400fc72c23d33702f
Jan Zernisch <jan.zernisch@invision.de>
2013-11-09 00:00:12 +0100

    Refactor Problem3

--- problem3/Problem3.hs
+++ problem3/Problem3.hs
@@ -2,7 +2,7 @@ module Problem3 where
 
 solutionFor :: Int -> Int
 solutionFor n
-  | smallestPrimeFactor == n = n
-  | otherwise            = solutionFor $ div n smallestPrimeFactor
+  | smallestPrimeFactor < n = solutionFor $ div n smallestPrimeFactor
+  | otherwise               = n
   where smallestPrimeFactor = head [x|x <- [2..n], mod n x == 0]
 
